d7fd1a6e4e6002efa49172a0a404866f860475b1
debabin <dmitriybabinup@gmail.com>
2024-06-08 16:37:51 +0700

    main 🧊 add test e2e

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,12 @@
 import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:5173/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle('Vite + React + TS');
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.goto('http://localhost:5173/');
 });
